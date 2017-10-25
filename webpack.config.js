d643572866ea4fac92f016ebecc97c2c44fed60c
Valera Vladyko <amintas42@gmail.com>
2017-10-25 12:42:55 +0300

    Add webpack compatibility

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path')
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './lib/index.js',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'htmllint-bundle-webpack.js',
+    libraryTarget: 'var',
+    library: 'htmllint'
+    // umdNamedDefine: true
+  },
+  //target: 'webworker',
+
+  resolve: {
+    extensions: ['*', '.js', '.jsx', 'index.js', 'index.jsx', '.json', 'index.json']
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+
+  module: {
+    rules : [
+    ],
+    loaders: [
+        //{ test: /\.js$/, exclude: /node_modules/, loader: 'babel'},
+        { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel'},
+        //{ test: /\.css$/, exclude: /static/, loader: 'style!css'}
+    ]
+  }
+
+  //devtool: '#eval'
+}
